f0eba13918842a22f0db8b3fde367503b23cf26a
ikegami-tech <ikegami@toho-next.com>
2026-01-08 15:58:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ const mapConfig = {
 
             { id: "ZOOM(角)", name: "ZOOM(角)", top: 11.0, left: 0.0, width: 12.0, height: 7.2 },
 
-            { id: "ZOOM(中)", name: "ZOOM(中)", top: 0.0, left: 57.0, width: 12.0, height: 7.2 },
+            { id: "ZOOM(中)", name: "ZOOM(中)", top: 0.0, left: 56.0, width: 12.0, height: 7.0 },
 
-            { id: "会議室2", name: "会議室2", top: 15.0, left: 78.0, width: 21.0, height: 22.0 },
+            { id: "会議室2", name: "会議室2", top: 15.0, left: 79.0, width: 21.0, height: 22.5 },
 
-            { id: "会議室1", name: "会議室1", top: 50.0, left: 78.0, width: 21.0, height: 15.0 },
+            { id: "会議室1", name: "会議室1", top: 50.0, left: 79.0, width: 21.0, height: 10.0 },
 
-            { id: "応接室(8人)", name: "応接室(8人)", top: 70.0, left: 0.0, width: 22.5, height: 28.0 },
+            { id: "応接室(8人)", name: "応接室(8人)", top: 71.5, left: 0.0, width: 22.5, height: 28.5 },
 
-            { id: "応接室(6人)", name: "応接室(6人)", top: 77.0, left: 23.0, width: 22.0, height: 23.0 },
+            { id: "応接室(6人)", name: "応接室(6人)", top: 76.5, left: 22.6, width: 22.0, height: 21.0 },
         ]
     },
     6: {
